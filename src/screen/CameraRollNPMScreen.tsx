a9dedbede2c621d22bb76a9a411b069e9553a7ac
imasdurrani <demha.ahmed@gmail.com>
2020-09-25 11:46:45 +1000

    improved on the overlapping scrollview

--- src/screen/CameraRollNPMScreen.tsx
+++ src/screen/CameraRollNPMScreen.tsx
@@ -11,7 +11,9 @@ import CameraRoll, {PhotoIdentifier} from '@react-native-community/cameraroll';
 
 const CameraRollNPMScreen = () => {
   const [photos, setPhotos] = useState<PhotoIdentifier[]>();
+  const [zIndex, setZIndex] = useState<Boolean>(false);
   const _handleButtonPress = () => {
+    console.log('clicked');
     CameraRoll.getPhotos({
       first: 20,
       assetType: 'Photos',
@@ -23,20 +25,31 @@ const CameraRollNPMScreen = () => {
   };
   return (
     <View>
-      <View style={{position: 'absolute', width: '100%', alignSelf: 'center'}}>
+      <View
+        style={{
+          /* paddingTop: 10, */
+          position: 'absolute',
+          width: '100%',
+          alignSelf: 'center',
+          zIndex: 0/* photos?.length === 0 ? 0 : 1 */,
+        }}>
         <Button title="Load Images" onPress={_handleButtonPress} />
       </View>
       <ScrollView
-        style={{paddingTop: 30,}}
+        style={{paddingTop: 30, zIndex: 1/* photos?.length !== 0 ? 1 : 0 */}}
         onScroll={(event) => console.log(event.nativeEvent.contentOffset)}>
         {photos?.map((p, i) => {
+          console.log(p.node.image.uri);
           return (
             <View>
               <Image
                 key={i}
-                style={{width: '100%', height: 500}}
+                style={{width: '100%', height: 500, zIndex: 1}}
                 source={{uri: p.node.image.uri}}
               />
+              {/* <Image   style={{width: '100%', height: 500, zIndex: 1}} source={{uri: 'file:///storage/emulated/0/Pictures/Title (1).jpg'}}/>
+              <Image   style={{width: '100%', height: 500, zIndex: 1}} source={{uri: 'file:///storage/emulated/0/Pictures/Title (1).jpg'}}/>
+              <Image   style={{width: '100%', height: 500, zIndex: 1}} source={{uri: 'file:///storage/emulated/0/Pictures/Title (1).jpg'}}/> */}
             </View>
           );
         })}
